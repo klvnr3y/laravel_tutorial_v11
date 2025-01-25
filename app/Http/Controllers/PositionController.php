b29d75f6d8301cd3dcae27a2f247cf61492ec066
Klaven Rey Matugas <klavenr3y.matugas@gmail.com>
2025-01-25 12:20:12 +0800

    asdsa

---
+++ app/Http/Controllers/PositionController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Position;
+use Illuminate\Http\Request;
+
+class PositionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Position $position)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Position $position)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Position $position)
+    {
+        //
+    }
+}
